86622a2bb3300c1e6465ba52bfe59796631944a6
David Requeno <davidrequeno52@gmail.com>
2024-02-04 21:51:22 -0600

    .env

--- http.js
+++ http.js
@@ -1,13 +1,15 @@
 const http = require('node:http')
 const { findAvailablePort } = require('./free-port')
 
+const desirePort = process.env.PORT ?? 3000
+
 const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hola mundo')
 })
 
-findAvailablePort(1234).then(port => {
+findAvailablePort(desirePort).then(port => {
   server.listen(port, () => {
-    console.log(`server listening on server http:localhost:${server.address().port}`)
+    console.log(`server listening on server http://localhost:${port}`)
   })
 })
